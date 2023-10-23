8fef20376978778b58761a0b32ebcb4e927437fe
Gen Ichino <zenoplex@gmail.com>
2023-10-24 00:33:40 +0900

    feat: add type inferable split

---
+++ apps/server/src/utils.ts
@@ -0,0 +1,15 @@
+// https://www.typescriptlang.org/play?ts=4.1.0-dev.20201028#example/string-manipulation-with-template-literals
+
+type Split<T extends string, S extends string> =
+  T extends `${infer U}${S}${infer V}`
+    ? string extends U
+      ? [U]
+      : [U, ...Split<V, S>]
+    : [T];
+
+export const split = <T extends string, S extends string>(
+  value: T,
+  separator: S
+): Split<T, S> => {
+  return value.split(separator) as Split<T, S>;
+}
