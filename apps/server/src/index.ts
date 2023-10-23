06985db0e7207e8453c1694d531b03d16e56a723
Gen Ichino <zenoplex@gmail.com>
2023-10-24 00:31:53 +0900

    chore: remove mock packages

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,14 @@
 import { ApolloServer } from 'apollo-server';
-import { addMocksToSchema } from '@graphql-tools/mock';
-import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
+import { db } from './db';
 
 const server = new ApolloServer({
-  schema: addMocksToSchema({
-    schema: makeExecutableSchema({ typeDefs, resolvers }),
-  }),
+  typeDefs,
+  resolvers,
+  context: { db}
 });
 
 const { url } = await server.listen();
+// eslint-disable-next-line no-console -- display server url
 console.log(`🚀  Server ready at ${url}`);
