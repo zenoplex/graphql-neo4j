b2947f849e035a890cb7db37287fe95529677409
Gen Ichino <zenoplex@gmail.com>
2023-10-23 23:54:11 +0900

    refactor: use mocks

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,13 @@
 import { ApolloServer } from 'apollo-server';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: { db : {}}
+  schema: addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+  }),
 });
 
 const { url } = await server.listen();
