24aaffa4e0a101879deb7e876f1b2579f94b9dcb
Gen Ichino <zenoplex@gmail.com>
2023-10-27 00:19:45 +0900

    chore: update schema

--- apps/server/src/schema.ts
+++ apps/server/src/schema.ts
@@ -1,49 +1,55 @@
 export const typeDefs = `#graphql
+  type Query {
+    fuzzyBusinessByName(searchString: String): [Business!]!
+      @cypher(
+        statement: """
+        CALL db.index.fulltext.queryNodes( 'businessNameIndex', $searchString+'~')
+        YIELD node RETURN node
+        """
+      )
+  }
 
-type Business {
-  businessId: ID!
-  name: String
-  address: String
-  avgStars: Float
-  photos(first: Int = 3, offset: Int = 0): [Photo!]!
-  reviews(first: Int = 3, offset: Int = 0): [Review!]!
-}
+  type Business {
+    businessId: ID!
+    waitTime: Int! @computed
+    averageStars: Float!
+      @cypher(
+        statement: "MATCH (this)<-[:REVIEWS]-(r:Review) RETURN avg(r.stars)"
+      )
+    recommended(first: Int = 1): [Business!]!
+      @cypher(
+        statement: """
+        MATCH (this)<-[:REVIEWS]-(:Review)<-[:WROTE]-(:User)-[:WROTE]->(:Review)-[:REVIEWS]->(rec:Business)
+        WITH rec, COUNT(*) AS score
+        RETURN rec ORDER BY score DESC LIMIT $first
+        """
+      )
+    name: String!
+    city: String!
+    state: String!
+    address: String!
+    location: Point!
+    reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
+    categories: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
+  }
 
-type User {
-  userId: ID!
-  name: String
-  photos: [Photo!]!
-  reviews: [Review!]!
-}
+  type User {
+    userID: ID!
+    name: String!
+    reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
+  }
 
-type Photo {
-  business: Business!
-  user: User!
-  photoId: ID!
-  url: String
-}
+  type Review {
+    reviewId: ID!
+    stars: Float!
+    date: Date!
+    text: String
+    user: User! @relationship(type: "WROTE", direction: IN)
+    business: Business! @relationship(type: "REVIEWS", direction: OUT)
+  }
 
-type Review {
-  reviewId: ID!
-  stars: Float
-  text: String
-  user: User!
-  business: Business!
-}
-
-type Query {
-  allBusinesses(first: Int = 10, offset: Int = 0): [Business!]!
-  businessBySearchTerm(
-    search: String!
-    first: Int = 10
-    offset: Int = 0
-    orderBy: BusinessOrdering = name_asc
-  ): [Business!]!
-  userById(userId: ID!): User
-}
-
-enum BusinessOrdering {
-  name_asc
-  name_desc
-}
+  type Category {
+    name: String!
+    businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
+  }
 `;
