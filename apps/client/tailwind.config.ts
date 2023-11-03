4494a2959fe9f791a52b5007d1a34958633363e1
Gen Ichino <zenoplex@gmail.com>
2023-11-03 22:32:51 +0900

    chore: update tailwind config

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import formPlugin from "@tailwindcss/forms";
 
 export default {
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-} satisfies Config
+  plugins: [formPlugin],
+} satisfies Config;
