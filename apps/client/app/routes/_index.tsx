fb9b0660697e19b391d1419d3a21cb8ed2a7b44a
Gen Ichino <zenoplex@gmail.com>
2023-11-06 22:44:27 +0900

    refactor: update app layout

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { Input } from "ui/input";
+import { Select, Label } from "ui";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,40 +8,48 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-const people = [
+const businesses = [
   {
-    name: "Lindsay Walton",
-    title: "Frontaaa-Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
+    businessId: "b1",
+    name: "San Mateo Public Library",
+    address: "55 W 3rd Ave",
+    category: "Library",
   },
-  // More people...
+  {
+    businessId: "b2",
+    name: "Ducky's Car Wash",
+    address: "716 N San Mateo Dr",
+    category: "Car Wash",
+  },
+  {
+    businessId: "b3",
+    name: "Hanabi",
+    address: "723 California Dr",
+    category: "Restaurant",
+  },
 ];
 
 export default function Index() {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <Input />
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Users
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Add user
-          </button>
-        </div>
-      </div>
+    <div className="grid grid-flow-row px-4 sm:px-6 lg:px-8 gap-5">
       <div className="mt-8 flow-root">
+        <h1 className="text-base font-semibold leading-6 text-gray-900 mb-5">
+          Business Search
+        </h1>
+        <Label htmlFor="business_category">Select Business Category</Label>
+        <Select id="business_category">
+          <option value="all">All</option>
+          <option value="library">All</option>
+          <option value="restaurant">restaurant</option>
+          <option value="car wash">car wash</option>
+        </Select>
+      </div>
+      <div className="sm:flex sm:items-center">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">
+          Results
+        </h1>
+      </div>
+      <div className="flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
@@ -58,50 +66,27 @@ export default function Index() {
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Title
+                      Address
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Role
-                    </th>
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                    >
-                      <span className="sr-only">Edit</span>
+                      Category
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                    <tr key={person.email}>
+                  {businesses.map((business) => (
+                    <tr key={business.name}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                        {person.name}
+                        {business.name}
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.title}
+                        {business.address}
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.email}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.role}
-                      </td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
+                        {business.category}
                       </td>
                     </tr>
                   ))}
